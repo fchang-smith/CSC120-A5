48e6b7e7d659c3ad3ac74b54bb631828c326e0f9
Feiran Chang <fchang@smith.edu>
2023-03-07 19:35:16 -0500

    alala

--- Engine.java
+++ Engine.java
@@ -1,3 +1,43 @@
 public class Engine {
 
+    private FuelType fuelType;
+    private double currentFuel;
+    private double maxFuel;
+
+    public Engine (FuelType f, double max){
+        fuelType = f;
+        maxFuel = max;
+        currentFuel = maxFuel;
+    }
+
+    public void refuel () {
+        currentFuel = maxFuel;
+    }
+
+    public void go () {
+        this.currentFuel -= 60;
+        this.checkFuel();
+        System.out.println("The engine is working! The current fuel level is: " + this.currentFuel);       
+    }
+
+    public void checkFuel () {
+        if (this.currentFuel < 0) {
+            throw new RuntimeException("Out of fuel!");
+        }
+    }
+
+    public double getCurrentFuel() {
+        return currentFuel;
+    }
+    public double getMaxFuel() {
+        return maxFuel;
+    }
+
+
+
+
+
+
+
+
 }
