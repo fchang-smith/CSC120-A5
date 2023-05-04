e49a27887d27dacf1ce6c59a34a4995e1916be97
Feiran Chang <fchang@smith.edu>
2023-05-03 20:27:37 -0400

    k

--- Car.java
+++ Car.java
@@ -1,14 +1,52 @@
 import java.util.ArrayList;
 
 public class Car {
-    ArrayList<String> passengerList;
+    ArrayList<Passenger> passengerList;
     int maxCapacity;
 
     public Car (int maxPassenger){
         this.maxCapacity = maxPassenger;
-        this.passengerList = new ArrayList<String>(maxCapacity);
+        this.passengerList = new ArrayList<Passenger>(maxCapacity);
     }
 
+    public int getCapacity() {
+        return this.maxCapacity;
+    }
 
+    public int seatsRemaining() {
+        int seatTaken = this.passengerList.size();
+        return this.maxCapacity - seatTaken;
+    }
+
+    public void addPassenger(Passenger p) {
+        int seatRemain = this.seatsRemaining();
+        if (seatRemain >= 1) {
+            this.passengerList.add(p);
+        } else {
+            throw new RuntimeException("The car is at its max capacity!");
+        }
+    }
+
+    public void removePassenger(Passenger p) {
+        if (this.passengerList.contains(p)) {
+            this.passengerList.remove(p);
+        } else {
+            throw new RuntimeException("There is no passenger " + p + " in the car");
+        }
+    }
+
+    public void printManifest() {
+        if (this.passengerList.isEmpty()) {
+            System.out.println("This car is EMPTY. ");
+        } else {
+            for (Passenger p : this.passengerList) {
+                System.out.println(this.passengerList.indexOf(p) + ": " + p);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
 
 }
